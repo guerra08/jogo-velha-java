c042f9e681aa982fe8829f545f191a476c0d2df7
Bruno Guerra <gguerrabruno@outlook.com>
2019-02-17 19:30:02 -0300

    first changes on fxml, logic testing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,52 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
 public class Controller {
+
+    @FXML
+    private GridPane myGrid;
+    @FXML
+    private Text txt0;
+    @FXML
+    private Text txt1;
+    @FXML
+    private Text txt2;
+    @FXML
+    private Text txt3;
+    @FXML
+    private Text txt4;
+    @FXML
+    private Text txt5;
+    @FXML
+    private Text txt6;
+    @FXML
+    private Text txt7;
+    @FXML
+    private Text txt8;
+
+    @FXML
+    private void initialize(){
+        txt0.setVisible(false);
+        txt1.setVisible(false);
+        txt2.setVisible(false);
+        txt3.setVisible(false);
+        txt4.setVisible(false);
+        txt5.setVisible(false);
+        txt6.setVisible(false);
+        txt7.setVisible(false);
+        txt8.setVisible(false);
+    }
+
+    @FXML
+    private void playOnTurn(){
+
+    }
+
+    private void changeText(Integer col, Integer row){
+
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,8 +8,11 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private Controller controller;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        controller = new Controller();
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 600));
